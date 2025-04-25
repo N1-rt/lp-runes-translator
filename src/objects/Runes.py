d9ceff577b9ac60a69d2a2fe9acd110ece84b179
N1r7 <n1rtottu5@proton.me>
2025-04-25 19:57:18 -0300

    1.0.0

---
+++ src/objects/Runes.py
@@ -0,0 +1,129 @@
+"""
+Version:1.0.1
+MadeBy:n1r7
+
+"""
+
+import os
+from pathlib import Path
+
+
+class Runes:
+    def __init__(self, runes_text: str):
+        self.runes_text = runes_text  # variavel onde as runas vão ser armazenadas
+        self.shift = 0  # obtendo quanto de shifting a pessoa vai querer nas runas
+
+    # lista das letras ; runas ; numeros correspondentes de cada runa para fazer a tradução
+    alphabet: list[str] = []
+    primus: list[int] = []
+    runes: list[str] = []
+    translated_text = ""
+    translated_text_without_shift = ""
+    inverted_runes = -1
+
+    # caminhos dos assets
+    PATH = Path.cwd()
+    ALPHABET_PATH = os.path.join(PATH.parent, "assets/alph.txt")
+    PRIMUS_PATH = os.path.join(PATH.parent, "assets/primus.txt")
+    RUNES_PATH = os.path.join(PATH.parent, "assets/runes.txt")
+
+    rune_shift = None
+    for letter in open(ALPHABET_PATH, "r"):
+        alphabet.append(letter.strip())
+        pass
+    for num in open(PRIMUS_PATH, "r"):
+        primus.append(int(num.strip()))
+        pass
+    for rune in open(RUNES_PATH, "r"):
+        runes.append(rune.strip())
+
+    # decifrando as runas
+    def decipher_rune(self):
+        try:
+            # resetando as variaveis antes de inserir algo nelas
+            self.translated_text = ""
+            self.translated_text_without_shift = ""
+            # obtendo o valor de cada runa no texto inserido!
+            for r in self.runes_text:
+                if (
+                    r.strip() in Runes.runes
+                ):  # removendo possiveis "/n" no texto e verificando se a runa esta presente na lista
+                    rune_index = Runes.runes.index(
+                        r
+                    )  # obtendo o valor do index na runa
+
+                    # transcrevendo o valor da runa para o valor do caractere baseado na posição
+                    # caso 1: se o valor do shift + o valor do index da runa for menor ou igual à 28(quantia de runas)
+                    # entao: valor da runa traduzida vai ser o valor do seu index + o shift no na letra do alfabeto correspondente
+                    # caso 2: valor do index da runa + shift é maior que 28(quantia de runas)
+                    # então: valor da runa traduzida vai ser o resto da divisão desse valor por 28(como dar a volta em um relogio e começar a contar denovo)
+                    rune_shift = (
+                        Runes.alphabet[rune_index + self.shift]
+                        if rune_index + self.shift <= 28
+                        else Runes.alphabet[(rune_index + self.shift) % 28]
+                    )
+                    # adicionar esses valores na variavel de texto
+                    self.translated_text_without_shift += Runes.alphabet[rune_index]
+                    self.translated_text += rune_shift
+
+                elif r == "•":
+                    # caso o caractere for um ponto,então adicionar caractere de um espaço ao texto decodificado
+                    self.translated_text += " "
+                    self.translated_text_without_shift += " "
+                else:
+                    # se o caractere nao estiver na lista de runas
+                    # exibir um erro!
+                    print(f"Rune({r}) not founded <:(")
+            # exibindo valores para o usuario
+            print("#" * 10 + " Translated Runes " + "#" * 10)
+            print(f"Original text:\n{self.runes_text}")
+            print(f"Original Translated text:\n{self.translated_text_without_shift}")
+            print(
+                f"Translated text With shift:{self.translated_text}"
+            ) if self.shift != 0 else print("No shift included!")
+            print(f"Shift value:{self.shift}")
+        except Exception as e:
+            raise e
+
+    def sum_gematria_values(self):
+        # WIP
+        for r in self.runes_text:
+            if r.strip() in Runes.runes:
+                rune_index = Runes.rune.index(r)
+
+    def set_shift_cipher(self, shift_num):
+        # auto-explicativo
+        self.shift = int(shift_num)
+
+    def invert_runes_using_atbash(self):
+        # decifrando texto usanto atbash: a = z ; b = y = c = x idem...
+        self.inverted_runes = self.inverted_runes * -1
+        rune_atbash_translated = ""
+        inverted_runes_text = ""
+        for r in self.runes_text:
+            # mesmo processo do metodo decipher
+            if r.strip() in Runes.runes:
+                rune_index = Runes.runes.index(r)
+                rune_atbash = Runes.alphabet[-rune_index - 1]
+                inverted_runes_text += Runes.runes[-rune_index - 1]
+                rune_atbash_translated += rune_atbash
+                print(f"Rune:{r}:{rune_index}:{Runes.alphabet[rune_index]}")
+            elif r.strip() == "•":
+                rune_atbash_translated += " "
+                inverted_runes_text += "•"
+        # substituindo os valores invertidos para a lista de runas e lista de texto traduzido para que possa
+        # ser feito shifiting e outras operações!
+        self.translated_text = rune_atbash_translated
+        self.runes_text = inverted_runes_text
+        # exibindo valores para usuario
+        print(f"Translated atbash runes text:\n{rune_atbash_translated}")
+        print(
+            "Warning the text now is inverted,if you want to back to normal use the command again"
+        )
+        print(f"Inverted Runes status:{self.inverted_runes}")
+        pass
+
+    def show_entered_text(self):
+        # auto-explicativo
+        print(f"entered translated text:\n{self.translated_text}")
+        print(f"entered runes:\n{self.runes_text}")
---
+++ src/shell.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+from os import system
+from os import path
+from objects import Runes
+
+# COMMANDS
+COMMAND_SC: str = "!"
+EXIT_COMMAND: str = COMMAND_SC + "exit"
+SHELL_VERSION: str = "1.0.0"
+CLEAR_COMMAND: str = COMMAND_SC + "clear"
+COMMAND_ENTER_TEXT: str = COMMAND_SC + "et"
+PRINT_ENTERED_TEXT: str = COMMAND_SC + "pet"
+TRANSLATE_RUNES: str = COMMAND_SC + "tr"
+SHIFT_RUNE = COMMAND_SC + "sr"
+
+COMMANDS = [
+    EXIT_COMMAND,
+    SHELL_VERSION,
+    CLEAR_COMMAND,
+    COMMAND_ENTER_TEXT,
+    PRINT_ENTERED_TEXT,
+    TRANSLATE_RUNES,
+    SHIFT_RUNE,
+]
+
+
+enter_text: bool = False
+entered_text: str = ""
+print("Starting shell...")
+try:
+    print(f"Shell started!\nShell version:{SHELL_VERSION}")
+    while True:
+        # pegando o input do usuario
+        cmd = input("->>>>:").strip()
+        # verificando se começa com o caractere de comandos
+        if not cmd.startswith(COMMAND_SC) and not enter_text:
+            print("ERROR:ENTER A VALID COMMAND!")
+        else:
+            match cmd:
+                case "!clear":
+                    system("clear")
+                case "!exit":
+                    print("EXITING SHELL!!!")
+                    break
+                case "!et":
+                    # enter texto
+                    file_or_shell = input(
+                        "Type if the text is in a file(f)\nor if you wanna type in shell(s)\n(f/s):"
+                    ).strip()
+                    if file_or_shell == "s":
+                        # se o texto for inserido no shell pegue o input e jogue no objeto Runes
+                        entered_text = input("TEXT:")
+                        ru = Runes.Runes(entered_text)
+                    elif file_or_shell == "f":
+                        string_text = ""
+                        file_path = input("type the file path:")
+                        is_valid_path = (
+                            print("Processing...\nfile founded!! >:D")
+                            if path.lexists(file_path)
+                            else print("File not founded\ncheck file path again!!")
+                        )
+                        for l in open(file_path, "r"):
+                            string_text += l
+                            string_text += "•"
+                            pass
+                        entered_text = string_text
+                        ru = Runes.Runes(entered_text)
+                    else:
+                        print("ERROR!!:TYPE A VALID COMMAND >:(")
+                case "!tr":
+                    # traduzir as runas
+                    if len(entered_text) != 0:
+                        try:
+                            ru.decipher_rune()
+                        except Exception as e:
+                            print("ERROR,cound't translate runes!!")
+                            raise e
+                case "!sr":
+                    # inserir um shift nas runas
+                    print("#" * 10 + " SETTING SHIFT TO RUNES " + "#" * 10 + "\n")
+                    number_shift = input("Type the number for shifting the runes:")
+                    ru.set_shift_cipher(int(number_shift))
+                case "!help":
+                    for command in COMMANDS:
+                        print(command)
+                case "!invert":
+                    # inverter runas
+                    print("#" * 10 + " TRANSLATED RUNES INVERTED " + "#" * 10 + "\n")
+                    ru.invert_runes_using_atbash()
+                case "!st":
+                    # exibir as runas e to texto traduzido
+                    ru.show_entered_text()
+
+                case _:
+                    print("command not founded")
+except Exception as e:
+    print("ERROR!:cound't start/continue shell operation\n ABORTING!!")
+    raise e
